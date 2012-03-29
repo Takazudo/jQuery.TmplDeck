28bfcc9b37afb152931873e188ab11c9dccda6e1
Takazudo <takazudo@gmail.com>
2012-03-29 23:21:59 +0900

    refactored with coffeescript

---
+++ README.md
@@ -0,0 +1,35 @@
+# jQuery.TmplDeck
+
+is a template string storage. useful with underscore.js's template method.  
+
+## Usage
+
+See demos
+
+## Demos
+
+* [demo](http://takazudo.github.com/jQuery.TmplDeck/demo/)
+
+## Depends
+
+[jQuery 1.7.1](http://jquery.com/)  
+[Underscore.js](http://documentcloud.github.com/underscore/) (easily replacable)
+
+## Browsers
+
+IE6+ and other new browsers.  
+
+## License
+
+Copyright (c) 2012 "Takazudo" Takeshi Takatsudo  
+Licensed under the MIT license.
+
+## Misc
+
+This scirpt was developed with following things.  
+
+ * [CoffeeScript][coffeescript]
+ * [grunt][grunt]
+
+[coffeescript]: http://coffeescript.org/ "CoffeeScript"
+[grunt]: https://github.com/cowboy/grunt "grunt"
--- jquery.tmpldeck.js
+++ jquery.tmpldeck.js
@@ -1,67 +1,59 @@
-/*!
- * jQuery.TmplDeck
- *
- * @author    : Takeshi Takatsudo (takazudo[at]gmail.com)
- * @copyright : Takeshi Takatsudo
- * @license   : The MIT License
- * @link      : https://github.com/Takazudo/jQuery.TmplDeck
- * @modified  : 2011/12/02
- * @version   : 0.1
- *
- * underscore.js template storage
- */
-(function($, window, document, ns, undefined){
+/*! jQuery.TmplDeck - v0.2 -  3/29/2012
+ * https://github.com/Takazudo/jQuery.TmplDeck
+ * Copyright (c) 2012 "Takazudo" Takeshi Takatsudo; Licensed MIT */
 
-ns.TmplDeck = function(url, options){
-	if (!(this instanceof arguments.callee)){
-		return new ns.TmplDeck(url, options);
-	}
-	this._options = $.extend({}, this._options, options, {
-		url: url
-	});
-	this._fetchDefer = $.Deferred();
-	this._cache = {};
-};
-ns.TmplDeck.prototype = {
-	_options: {
-		dataType: 'text',
-		url: null,
-		cache: true
-	},
-	_fetchedText: null,
-	_fetchDefer: null,
-	_cache: null,
-	load: function(){
-		var self = this;
-		return $.ajax(self._options).then(function(data){
-			self._fetchedText = data;
-			self._fetchDefer.resolve();
-		}, function(){
-			alert('ajax failed');
-		});
-	},
-	ready: function(fn){
-		this._fetchDefer.done(function(){
-			fn();
-		});
-	},
-	draw: function(id){
-		if(this._cache[id]){
-			ret =  this._cache[id];
-		}else{
-			var re = new RegExp('(^|\\n)' + id + '\\s\{\{\{\\r?\\n([\\s\\S]+?)\}\}\}');
-				// get text inside parenthesis
-			var res = this._fetchedText.match(re);
-			ret = !res ? null : (this._cache[id] = res[2]);
-		}
-		return ret;
-	}
-};
+(function() {
+  var TmplDeck;
 
-/* underscore.js enhancement */
+  (function($) {}, TmplDeck = (function() {
 
-ns.TmplDeck.prototype.tmpl = function(id, data){
-	return _.template(this.draw(id))(data);
-};
+    TmplDeck.prototype._options = {
+      dataType: 'text',
+      url: null,
+      cache: true
+    };
 
-})(jQuery, this, this.document, jQuery);
+    function TmplDeck(url, options) {
+      if (!(this instanceof arguments.callee)) return new TmplDeck(url, options);
+      this._options = $.extend({}, this._options, {
+        url: url
+      });
+      this._fetchDefer = $.Deferred();
+      this._cache = {};
+    }
+
+    TmplDeck.prototype.load = function() {
+      var _this = this;
+      return ($.ajax(this._options)).then(function(data) {
+        _this._fetchedText = data;
+        return _this._fetchDefer.resolve();
+      });
+    };
+
+    TmplDeck.prototype.ready = function(fn) {
+      this._fetchDefer.done(function() {
+        return fn();
+      });
+      return this;
+    };
+
+    TmplDeck.prototype.draw = function(id) {
+      var cached, re, res;
+      cached = this._cache[id];
+      if (cached) return cached;
+      re = new RegExp("(^|\\n)" + id + "\\s\{\{\{\\r?\\n([\\s\\S]+?)\}\}\}");
+      res = this._fetchedText.match(re);
+      if (res) {
+        return (this._cache[id] = res[2]);
+      } else {
+        return null;
+      }
+    };
+
+    return TmplDeck;
+
+  })(), TmplDeck.prototype.tmpl = function(id, data) {
+    return (_.template(this.draw(id)))(data);
+  }, $.TmplDeck = TmplDeck)(jQuery);
+
+}).call(this);
